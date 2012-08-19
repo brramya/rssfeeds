81f77b3bc013c870e87dc2d2d6f8b68e64739e17
ramya <brramya@gmail.com>
2012-08-19 17:12:51 -0400

    Added TagDetailsModel

---
+++ src/java/edu/stratford/rbandlar8760/rssfeeds/model/TagDetailsModel.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.stratford.rbandlar8760.rssfeeds.model;
+
+import edu.stratford.rbandlar8760.rssfeeds.db.TagDetails;
+import edu.stratford.rbandlar8760.rssfeeds.db.Website;
+import java.util.HashMap;
+import java.util.List;
+import org.apache.cayenne.CayenneRuntimeException;
+import org.apache.cayenne.exp.Expression;
+import org.apache.cayenne.query.SelectQuery;
+
+/**
+ *
+ * @author RamyaNari
+ */
+public class TagDetailsModel extends BaseModel{
+
+   public List<TagDetails> listTagDetailsForWebsite( Website w ) throws ModelException
+    {
+        try
+        {
+            Expression qualifier = Expression.fromString( "toWebsite = $website" );
+            final HashMap<String, Object> map = new HashMap<String, Object>( 2 );
+            map.put( "website", w );
+            qualifier = qualifier.expWithParameters( map );
+
+            final SelectQuery query = new SelectQuery( TagDetails.class, qualifier );
+            @SuppressWarnings("unchecked")
+            final List<TagDetails> td = dataContext.performQuery( query );
+            if (td.isEmpty())
+            {
+                throw new ModelException( "Could not list Tag Details" );
+            }
+            return td;
+        }
+        catch (CayenneRuntimeException ce)
+        {
+            throw new ModelException( ce.getMessage() );
+        }
+    }
+
+
+
+
+  public TagDetails create( Website w, String tag ) throws ModelException{
+
+      if ( tag == null || tag.length() == 0 )
+          throw new IllegalArgumentException("Invalid tag name");
+
+      if ( w == null )
+          throw new IllegalArgumentException("Invalid website");
+      try{
+          final TagDetails td = dataContext.newObject(TagDetails.class);
+            td.setToWebsite(w);
+            td.setTagname(tag);
+          dataContext.commitChanges();
+          return td;
+      }catch (CayenneRuntimeException ce)
+      {
+          dataContext.rollbackChanges();
+          throw new ModelException("Could not add Tag Details");
+      }
+  }
+}
